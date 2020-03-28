2611b4ddcb9a0d5f0454a412dcbd81ea1efd9e1d
kqhome <a11dc094@dhw.ac.jp>
2020-03-28 20:10:43 +0900

    add pract3 flagment 動いた

--- app/src/main/java/com/example/practiceandroidapp/Pract3Activity.kt
+++ app/src/main/java/com/example/practiceandroidapp/Pract3Activity.kt
@@ -2,11 +2,22 @@ package com.example.practiceandroidapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.fragment.app.FragmentManager
 
 class Pract3Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pract3)
+
+        val arr= arrayListOf("a","b","c","d")
+        val bind =Bundle()
+        bind.putString("param1","hoge")
+        bind.putString("param2",Math.random().toString())
+        bind.putStringArrayList("param3",arr)
+
+        supportFragmentManager.findFragmentById(R.id.Flagment1 )
+        findViewById<TextView>(R.id.flagment_text_1).setText(Math.random().toString())
     }
 }
