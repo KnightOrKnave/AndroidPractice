41ae7b4c1f68507dc84b55b64acdb941b1adc475
kqhome <a11dc094@dhw.ac.jp>
2020-03-28 14:29:22 +0900

    add pract1 画面遷移しない

--- app/src/main/java/com/example/practiceandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/practiceandroidapp/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.practiceandroidapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //set pract1 button
+        findViewById<View>(R.id.button1).setOnClickListener(View.OnClickListener {
+            fun onClick(view:View){
+                val context=view
+                    .context
+                    .startActivity(Intent(view.context,Pract1Activity::class.java))
+            }
+        })
     }
 }
